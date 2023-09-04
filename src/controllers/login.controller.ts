38a77b43822ceef91cf01ee3dd8cc392c7106a23
Jozhué Beni <jbeeniif@gmail.com>
2023-09-04 15:35:19 -0300

    Organization on Login Controller

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { LoginReq } from "../interfaces/login.interface";
 import { createLoginService } from "../services/login/createLoginService.service";
 
+// passível de mudanças
+
 export const createLoginController = async (
   req: Request,
   res: Response
